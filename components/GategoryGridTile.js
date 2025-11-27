4f39fce9bf4c85ec0203109ffba796d90a640a87
TetianaKlitna <klitna.tetiana@gmail.com>
2025-11-26 22:16:18 -0800

    CategoriesScreen

--- components/GategoryGridTile.js
+++ components/GategoryGridTile.js
@@ -1,15 +1,52 @@
-import { Pressable, View, Text } from "react-native";
+import { Pressable, View, Text, StyleSheet, Platform } from "react-native";
 
 function CategoryGridTile({ title, color }) {
     return (
-        <View>
-          <Pressable>
-            <View>
-              <Text>{title}</Text>
-            </View>
-          </Pressable>
+        <View style={styles.gridItem}>
+            <Pressable style={({ pressed }) => [
+                styles.btn,
+                pressed && styles.pressed
+            ]} >
+                <View style={[styles.innerContainer, { backgroundColor: color }]}>
+                    <Text style={styles.title}>{title}</Text>
+                </View>
+            </Pressable>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    gridItem: {
+        flex: 1,
+        margin: 16,
+        height: 150,
+        borderRadius: 8,
+        elevation: 4,
+        backgroundColor: 'white',
+        shadowColor: 'black',
+        shadowOpacity: 0.25,
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 8,
+        overflow: Platform.OS === 'android' ? 'hidden' : 'visible',
+    },
+    btn: {
+        flex: 1,
+    },
+    innerContainer: {
+        flex: 1,
+        padding: 16,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 8,
+        overflow: Platform.OS === 'android' ? 'hidden' : 'visible',
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 18,
+    },
+    pressed: {
+        opacity: 0.5,
+    },
+});
 
 export default CategoryGridTile;
