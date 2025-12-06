18eb472d6a526de2566eaba3cf1fb93343cbe5dc
TetianaKlitna <klitna.tetiana@gmail.com>
2025-12-06 09:56:23 -0800

    FavoritiesScreen

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,10 +1,42 @@
-import { Text } from 'react-native';
+import { useContext } from 'react';
+import { View, FlatList, StyleSheet, Text } from 'react-native';
+import { MEALS } from '../data/dummy-data';
+import { FavoritesContext } from '../store/context/favorite-context';
+import MealItem  from '../components/MealItem';
 
 function FavoritesScreen() {
+  const favoriteMealsCtx = useContext(FavoritesContext);
+  const favoriteMeals = MEALS.filter((meal) => favoriteMealsCtx.ids.includes(meal.id));
+
+  if (favoriteMeals.length === 0) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>You have no favorite meals yet.</Text>
+      </View>
+    );
+  }
+
   return (
-    <>
-      <Text>Your FavoritesScreen content goes here</Text>
-    </>
+    <FlatList
+      data={favoriteMeals}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => <MealItem item={item} />}
+    />
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,    
+  },
+    text: {
+      fontSize: 16,
+      color: 'black',
+      fontWeight: 'bold',
+    },
+});
+
 export default FavoritesScreen;
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,23 +1,36 @@
-import { useLayoutEffect } from "react";
+import { useLayoutEffect, useContext } from "react";
 import { View, Text, StyleSheet, ScrollView, Button } from "react-native";
 import { MEALS } from "../data/dummy-data";
 import MealItemDetail from "../components/MealItemDetail";
 import MealImage from "../components/MealImage";
 import SubTitle from "../components/SubTitle";
 import  IconButton from "../components/IconButton";
+import { FavoritesContext } from "../store/context/favorite-context";
 
 function MealDetailScreen({ route, navigation }) {
+    const favoriteMealsCtx = useContext(FavoritesContext);
     const mealId = route.params.mealId;
     const selectedMeal = MEALS.find((meal) => meal.id === mealId);
-    function infoButtonHandler() {
-        console.log("Info button pressed!");
+    const mealIsFavorite = favoriteMealsCtx.ids.includes(mealId);
+    function changeFavoriteStatusHandler() {
+        if (mealIsFavorite) {
+            favoriteMealsCtx.removeFavorite(mealId);
+        } else {
+            favoriteMealsCtx.addFavorite(mealId);
+        }
     }
     useLayoutEffect(() => {
         navigation.setOptions({
             title: selectedMeal.title,
-            headerRight: () => (<IconButton icon="star" color="white" onPress={infoButtonHandler} />),
+            headerRight: () => (
+                <IconButton
+                    icon={mealIsFavorite ? "star" : "star-outline"}
+                    color="white"
+                    onPress={changeFavoriteStatusHandler}
+                />
+            ),
         });
-    }, [mealId, navigation, infoButtonHandler]);
+    }, [mealId, navigation, changeFavoriteStatusHandler, mealIsFavorite]);
     return (
         <ScrollView>
             <View style={{ flex: 1, padding: 16 }}>
