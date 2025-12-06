c10da5b88b2562dbab4442c1a12cd26a02e98dc0
TetianaKlitna <klitna.tetiana@gmail.com>
2025-12-05 23:22:50 -0800

    Drawer

--- App.js
+++ App.js
@@ -1,16 +1,49 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import CategoriesScreen from './screens/CategoriesScreen';
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
 import MealDetailScreen from './screens/MealDetailScreen';
+import FavoritesScreen from './screens/FavoritesScreen';
+import { Ionicons } from '@expo/vector-icons';
 
 const Stack = createNativeStackNavigator();
+function DrawerNavigator() {
+  const Drawer = createDrawerNavigator();
+  return (
+    <Drawer.Navigator screenOptions={{
+      headerStyle: { backgroundColor: '#494848ff' },
+      headerTintColor: 'white',
+      sceneContainerStyle: { backgroundColor: '#e5e3e2ff' },
+      drawerContentStyle: { backgroundColor: '#494848ff' },
+      drawerInactiveTintColor: 'white',
+      drawerActiveTintColor: '#494848ff',
+      drawerActiveBackgroundColor: '#e5e3e2ff',
+    }}>
+      <Drawer.Screen
+        name="Categories"
+        component={CategoriesScreen}
+        options={{ 
+          title: 'All Categories', 
+          drawerIcon: ({ color, size }) => <Ionicons name="list" color={color} size={size} /> }}
+      />
+      <Drawer.Screen
+        name="Favorites"
+        component={FavoritesScreen}
+        options={{ 
+          title: 'Your Favorites',
+          drawerIcon: ({ color, size }) => <Ionicons name="star" color={color} size={size} /> }}
+      />
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: "#494848ff" }}>
+    <>
       <StatusBar style="light" backgroundColor="#494848ff" />
 
       <NavigationContainer>
@@ -24,8 +57,10 @@ export default function App() {
         >
           <Stack.Screen
             name="MealsCategories"
-            component={CategoriesScreen}
-            options={{ title: "All Categories" }}
+            component={DrawerNavigator}
+            options={{ 
+              headerShown: false,
+            }}
           />
 
           <Stack.Screen
@@ -39,11 +74,10 @@ export default function App() {
             component={MealDetailScreen}
             options={{
               headerBackVisible: true,
-            }
-            }
+            }}
           />
         </Stack.Navigator>
       </NavigationContainer>
-    </SafeAreaView>
+    </>
   );
 }
---
+++ screens/FavoritesScreen.js
@@ -0,0 +1,10 @@
+import { Text } from 'react-native';
+
+function FavoritesScreen() {
+  return (
+    <>
+      <Text>Your FavoritesScreen content goes here</Text>
+    </>
+  );
+}
+export default FavoritesScreen;
